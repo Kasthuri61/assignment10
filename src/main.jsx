f70c2a3d0644a7a124eb1edc970faf7a49d7fc34
Kasthuri61 <kasthurirevathi6165@gmail.com>
2025-07-04 22:41:12 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App'; 
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import App from './App.jsx'
 
-const root = createRoot(document.getElementById('root'));
-
-root.render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HashRouter>
     <App />
-  </StrictMode>
-);
-q
+  </HashRouter>
+)
